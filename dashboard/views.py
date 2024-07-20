afd1039ba83dac716f55fa8ebd3dfc4a1346d2e6
magickoo <magickhu@gmail.com>
2024-07-20 20:25:39 +0530

    feat: homework section added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.core.checks import messages
 from django.shortcuts import redirect, render
-from . forms import NoteForm
-from . models import Note 
+from . forms import DashboardForm, HomeworkForm, NoteForm, TodoForm, CreateUserForm
+from . models import Note, Homework, Todo
 from django.contrib import messages
 from django.views import generic
 #from youtubesearchpython import VideosSearch
@@ -49,6 +49,58 @@ def delete_note(request,pk=None):
     Note.objects.get(id=pk).delete()
     return redirect("notes")
 
-
 class NotesDetailView(generic.DetailView):
     model = Note
+    
+@login_required
+def homework(request):
+    if request.method == "POST":
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST['is_finished']
+                if finished == 'on':
+                    finished = True
+                else:
+                    finished = False
+            except:
+                finished = False
+            homeworks = Homework(
+                user = request.user,
+                subject = request.POST['subject'],
+                title = request.POST['title'],
+                description = request.POST['description'],
+                due = request.POST['due'],
+                is_finished = finished
+                
+            )
+            homeworks.save()
+            messages.success(request,f"Homework Added from {request.user.username} Successfully!")
+    else:     
+        form = HomeworkForm()
+    homework = Homework.objects.filter(user=request.user)
+    if len(homework) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+    context ={
+        'homeworks' : homework,
+        'homework_done' : homework_done,
+        'form' : form
+    }
+    return render(request,'dashboard/homework.html',context)
+
+@login_required
+def update_homework(request,pk=None):
+    homework = Homework.objects.get(id=pk)
+    if homework.is_finished == True :
+        homework.is_finished = False
+    else:
+        homework.is_finished = True
+    homework.save()
+    return redirect("homework")
+
+@login_required
+def delete_homework(request,pk=None):
+    Homework.objects.get(id=pk).delete()
+    return redirect("homework")
